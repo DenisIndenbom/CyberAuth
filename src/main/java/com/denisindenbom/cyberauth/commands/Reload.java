cbbe5edb06633b35ae6e6ef5ee6bb3bec92a78b6
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2021-12-15 17:37:40 +0300

    fix bugs added a new command to remove users from the database

--- src/main/java/com/denisindenbom/cyberauth/commands/Reload.java
+++ src/main/java/com/denisindenbom/cyberauth/commands/Reload.java
@@ -1,7 +1,6 @@
 package com.denisindenbom.cyberauth.commands;
 
 import com.denisindenbom.cyberauth.messagesender.MessageSender;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
